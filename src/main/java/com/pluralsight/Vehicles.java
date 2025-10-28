f0574fe65336379586c38dc8af876b2574afffe1
Alwin Zhao <alwinzhao12@gmail.com>
2025-10-28 16:00:44 -0400

    verify each Moped Car Semitruck and Hovercraft instances are children of Vehicle in main

--- src/main/java/com/pluralsight/Vehicles.java
+++ src/main/java/com/pluralsight/Vehicles.java
@@ -2,6 +2,11 @@ package com.pluralsight;
 
 public class Vehicles {
     public static void main(String[] args) {
+        Moped moped = new Moped("blue", 1, 1, 2);
+        Car car = new Car("black", 5, 5, 12);
+        Semitruck semitruck = new Semitruck("white", 3, 200, 50);
+        Hovercraft hovercraft = new Hovercraft("red", 8, 30, 15);
 
+        moped.setColor("green");
     }
 }
